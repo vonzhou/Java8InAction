499748c9e8723457aa67f9abdd16ca086f244257
vonzhou <vonzhou@163.com>
2017-11-09 19:34:12 +0800

    reading

--- src/main/java/lambdasinaction/chap3/Sorting.java
+++ src/main/java/lambdasinaction/chap3/Sorting.java
@@ -5,49 +5,54 @@ import static java.util.Comparator.comparing;
 
 public class Sorting {
 
-    public static void main(String...args){
+    public static void main(String... args) {
 
-        // 1
         List<Apple> inventory = new ArrayList<>();
-        inventory.addAll(Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red")));
+        inventory.addAll(Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red")));
 
-        // [Apple{color='green', weight=80}, Apple{color='red', weight=120}, Apple{color='green', weight=155}]
+        /**
+         * 1. 行为封装到类中
+         */
         inventory.sort(new AppleComparator());
         System.out.println(inventory);
 
         // reshuffling things a little
         inventory.set(1, new Apple(30, "green"));
-        
-        // 2
-        // [Apple{color='green', weight=30}, Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+
+        /**
+         * 2. 使用匿名类
+         */
         inventory.sort(new Comparator<Apple>() {
-            public int compare(Apple a1, Apple a2){
-                return a1.getWeight().compareTo(a2.getWeight()); 
-        }});
+            public int compare(Apple a1, Apple a2) {
+                return a1.getWeight().compareTo(a2.getWeight());
+            }
+        });
         System.out.println(inventory);
 
         // reshuffling things a little
         inventory.set(1, new Apple(20, "red"));
-        
-        // 3
-        // [Apple{color='red', weight=20}, Apple{color='green', weight=30}, Apple{color='green', weight=155}]
+
+        /**
+         * 3. 使用 Lambda
+         */
         inventory.sort((a1, a2) -> a1.getWeight().compareTo(a2.getWeight()));
         System.out.println(inventory);
-        
+
         // reshuffling things a little
         inventory.set(1, new Apple(10, "red"));
-        
-        // 4
-        // [Apple{color='red', weight=10}, Apple{color='red', weight=20}, Apple{color='green', weight=155}]
+
+        /**
+         * 4 更简洁，使用方法引用
+         */
         inventory.sort(comparing(Apple::getWeight));
-        System.out.println(inventory);       
+        System.out.println(inventory);
     }
 
     public static class Apple {
         private Integer weight = 0;
         private String color = "";
 
-        public Apple(Integer weight, String color){
+        public Apple(Integer weight, String color) {
             this.weight = weight;
             this.color = color;
         }
@@ -69,15 +74,12 @@ public class Sorting {
         }
 
         public String toString() {
-            return "Apple{" +
-                   "color='" + color + '\'' +
-                   ", weight=" + weight +
-                   '}';
+            return "Apple{" + "color='" + color + '\'' + ", weight=" + weight + '}';
         }
     }
 
     static class AppleComparator implements Comparator<Apple> {
-        public int compare(Apple a1, Apple a2){
+        public int compare(Apple a1, Apple a2) {
             return a1.getWeight().compareTo(a2.getWeight());
         }
     }
